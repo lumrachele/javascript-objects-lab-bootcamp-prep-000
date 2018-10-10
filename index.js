833d268cda04350fedd670a697a2bc264d0be217
Rachel Lum <rlum894@gmail.com>
2018-10-10 21:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function deleteFromObjectByKey (object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey (object, key){
-  var object[key] = delete object.key
+  var object = [key]
+  delete object.key
   return object
 }
